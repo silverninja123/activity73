bedc71d207964349ac4a69d5e6061d57bf10f2ec
silverninja123 <goldencrumbs27@gmail.com>
2026-02-21 19:43:49 +0500

    Add files via upload

---
+++ index.py
@@ -0,0 +1,6 @@
+age=int(input("What is your age :"))
+nationality=input("Are you from USA? (yes/no) :")
+if age>=18 and nationality=="yes":
+    print("You are eligible for vote")
+else:
+    print("You are not eligible for vote")
